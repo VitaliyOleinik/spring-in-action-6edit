00a3e1707e2f04ebd41ab7d2982331f3e416d961
Vitaliy Oleinik <oleynik624@gmail.com>
2023-04-18 22:51:58 +0600

    add table and id

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -2,23 +2,29 @@ package tacos;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class TacoOrder {
+@Table("Taco_Cloud_Order")
+public class TacoOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @Id
     private Long id;
 
     private Date placedAt;
 
+    @Column("customer_name")
     @NotNull(message = "Delivery name is required")
     private String deliveryName;
 
